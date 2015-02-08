9de95bf1e3e6018021ce0dabc690533d5b62909b
mvagru <mvagru@outlook.com>
2015-02-08 20:05:13 +0700

    Project 0.0.5

---
+++ Book C 5.0 CLR 4.5/App3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace App3
+{
+    public class UnitConverter
+    {
+        int ratio;  // Поле
+        public UnitConverter(int unitRatio) { ratio = unitRatio; } // Конструктор
+        public int Convert(int unit) { return unit * ratio; } // Метод
+    }
+    class Program
+    {
+        static void Main()
+        {
+            UnitConverter feetToInchesConverter = new UnitConverter(12); 
+            UnitConverter milesToFeetConverter = new UnitConverter(5280);
+            Console.WriteLine(feetToInchesConverter.Convert(30)); // 360
+            Console.WriteLine(feetToInchesConverter.Convert(100)); // 1200
+            Console.WriteLine(feetToInchesConverter.Convert(milesToFeetConverter.Convert(1))); // 5280*1*12=63360
+        }
+    }
+}
---
+++ Book C 5.0 CLR 4.5/App4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace App4
+{
+    public class Panda
+    {
+        public string Name; // Поле экземпляра
+        public static int Population; // Статическое поле
+        public Panda(string n) // Конструктор
+        {
+            Name = n; // Присвоить значение полю экземпляра 
+            Population = Population + 1; // Инкрементировать значение статического поля
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Panda p1 = new Panda("Pan Dee");
+            Panda p2 = new Panda("Pan Dah");
+            Console.WriteLine(p1.Name); // Pan Dee
+            Console.WriteLine(p2.Name); // Pan Dah
+            Console.WriteLine(Panda.Population); // 2
+        }
+    }
+}
