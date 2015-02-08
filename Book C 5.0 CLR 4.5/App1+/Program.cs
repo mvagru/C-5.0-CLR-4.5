efa5e71c2506bbb455cfa76b1ab4ecc67799bdde
mvagru <mvagru@outlook.com>
2015-02-08 19:18:05 +0700

    Project 0.0.4

--- Book C 5.0 CLR 4.5/App1+/Program.cs
+++ Book C 5.0 CLR 4.5/App1+/Program.cs
@@ -4,12 +4,12 @@ namespace App1_
 {
     class Program
     {
-        static void Main() /* Вызов метода FeetToInches с передачей аргументов для обработки */
+        static void Main() /* Вызов метода FeetToInches с передачей литералов (аргументов) для обработки */
         {
             Console.WriteLine(FeetToInches (30)); //360
             Console.WriteLine(FeetToInches (100)); //1200
         }
-        static int FeetToInches(int feet) /* Метод принимающий аргументы в футах*/
+        static int FeetToInches(int feet) /* Метод принимающий литералы (аргументы) в футах*/
         {
             int inches = feet * 12; // Конвертация футы в дюймы
             return inches; // Выходной аргумент возвращающий параметр в дюймах
---
+++ Book C 5.0 CLR 4.5/App2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string message = "Hello world";
+            string upperMessage = message.ToUpper();
+            Console.WriteLine(upperMessage);
+            int x = 2015;
+            message = message + x.ToString();
+            Console.WriteLine(message);
+            bool simpleVar = false;
+            if (simpleVar)
+                Console.WriteLine("This will not print");
+            int y = 5000;
+            bool lessThanAMile = y < 5280;
+            if (lessThanAMile)
+                Console.WriteLine("This will print");
+        }
+    }
+}
