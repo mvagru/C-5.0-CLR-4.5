93e0444cb171cc1fd8340c4844043683c7ebd81e
mvagru <mvagru@outlook.com>
2015-02-10 14:18:43 +0500

    Project 0.0.7

---
+++ Book C 5.0 CLR 4.5/App6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App6
+{
+    public class Point { int x, y; }
+    public struct Point2 { int x2, y2;}
+    class Program
+    {
+        static void Main()
+        {
+            int x = 1;
+            Point p = null;
+            Console.WriteLine(p == null); // Значение true
+            /* Ссылке может быть присвоен литерал null, который отражает тот факт, что ссылка не указывает ни один объект. */
+            Point2 p2 = null;
+            /* Тип значения обычно не может иметь значение null. Кроме типов допустающих значение null */
+        }
+    }
+}
